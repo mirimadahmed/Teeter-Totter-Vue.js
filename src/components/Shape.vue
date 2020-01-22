34f7144d45d28f291f617df3284a6a28a6c4f282
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-22 22:39:22 +0500

    basic shape component

---
+++ src/components/Shape.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="shape">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.shape {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform-origin: top left;
+}
+</style>
