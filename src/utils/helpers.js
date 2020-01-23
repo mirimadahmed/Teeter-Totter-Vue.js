e64a5aa3b9206abda81780920f507710166aaa54
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-23 12:31:56 +0500

    random color generator

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,6 +2,9 @@ const helpers = {
   generateRandomNumber(min = 0, max = 1) {
     return min + Math.round(Math.random() * max);
   },
+  generateRandomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  },
 };
 
 export default helpers;
