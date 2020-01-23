28aa2683ab4b441f9a17212abe8c52856f0c842f
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-23 12:34:19 +0500

    basic board structure

---
+++ src/components/Board.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="teeter-totter">
+    <div class="board">
+      <!-- Left shapes -->
+      <!-- Right shapes -->
+    </div>
+    <!-- Bottom center triangle -->
+    <div class="foundation"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
