ff51f897348292a5f36877c9be12661e0aa3fa36
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-23 13:16:04 +0500

    some controls shown in the hud

--- src/components/Hud.vue
+++ src/components/Hud.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
-<!-- Showing some controls for the game here -->
+  <div class="hud">
+    <!-- Showing some controls for the game here -->
+    <div class="user-shapes-stats">
+      <div>
+        Total weight: {{ leftShapesWeight  }}
+      </div>
+      <div>
+        Momentum: {{ fallingShapes[0] * currentSpeed }}
+      </div>
+    </div>
+    <div class="auto-shapes-stats">
+      <div>
+        Total weight: {{ rightShapesWeight }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex';
 
+export default {
+  computed: {
+    ...mapState([
+      'leftShapes',
+      'rightShapes',
+      'fallingShapes',
+      'currentSpeed',
+    ]),
+    leftShapesWeight() {
+      return this.totalWeight(this.leftShapes);
+    },
+    rightShapesWeight() {
+      return this.totalWeight(this.rightShapes);
+    },
+  },
+  methods: {
+    totalWeight(shapes = []) {
+      let total = 0;
+      shapes.forEach((element) => {
+        total += element.weight;
+      });
+      return total;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
+.hud {
+  display: flex;
+  justify-content: space-between;
+  flex: 0 0 6rem;
+  padding: 0.5rem 1.5rem;
+}
 </style>
