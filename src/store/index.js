2e6d79262d7560a1f187e85a1c0f524c6de9da52
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-23 12:55:27 +0500

    sum and get the board angle

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import helpers from '@/utils/helpers';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -21,6 +23,28 @@ export default new Vuex.Store({
     },
     addLeftShape(state, shape = {}) {
       state.leftShapes.push(shape);
+    },
+  },
+  getters: {
+    boardAngle({ leftShapesSum, rightShapesSum }) {
+      let angle = 0;
+
+      // calculate the angle of the board
+      const diff = Math.abs(leftShapesSum, rightShapesSum);
+      angle = leftShapesSum > rightShapesSum
+        ? diff / leftShapesSum * -50
+        : diff / rightShapesSum * 50;
+
+      return angle;
+    },
+    isBoardValid({ boardAngle }) {
+      return boardAngle > -30 && boardAngle < 30;
+    },
+    leftShapesSum({ leftShapes }) {
+      return helpers.getShapesSum(leftShapes, true);
+    },
+    rightShapesSum({ rightShapes }) {
+      return helpers.getShapesSum(rightShapes, false);
     },
   },
   actions: {
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,6 +5,16 @@ const helpers = {
   generateRandomColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
   },
+  getShapesSum(shapes = [], isLeft) {
+    return shapes.reduce((total, current) => {
+      const left = isLeft ? (50 - current.left) : current.left;
+
+      // eslint-disable-next-line no-param-reassign
+      total += current.weight * left;
+
+      return total;
+    }, 0);
+  },
 };
 
 export default helpers;
