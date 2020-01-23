4f76dae9159ad052e425fd6dee006ceba01b663c
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-23 12:42:13 +0500

    basic mutations created

--- src/store/index.js
+++ src/store/index.js
@@ -11,6 +11,17 @@ export default new Vuex.Store({
     gamePaused: true, // initially game will be paused
   },
   mutations: {
+    resetGame(state) {
+      state.leftShapes = [];
+      state.rightShapes = [];
+      state.droppingShapes = [];
+    },
+    toggleSimulation(state) {
+      state.gamePaused = !state.gamePaused;
+    },
+    addLeftShape(state, shape = {}) {
+      state.leftShapes.push(shape);
+    },
   },
   actions: {
   },
