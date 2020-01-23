175a1b4e31ba8e87484c9b34c886547b809d3d47
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-23 13:52:33 +0500

    show falling shapes

--- src/components/FallingShapes.vue
+++ src/components/FallingShapes.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-<!-- Shapes that are currently falling here -->
+  <div class="falling-shapes">
+    <!-- Shapes that are currently falling here -->
+    <Shape
+      v-for="shape in droppingShapes"
+      :key="shape.id"
+      :shape="shape"
+    />
   </div>
 </template>
 
 <script>
-export default {
+import { mapGetters, mapState, mapMutations } from 'vuex';
+import Shape from '@/components/Shape.vue';
 
+export default {
+  components: {
+    Shape,
+  },
+  computed: {
+    ...mapGetters(['boardAngle', 'isBoardValid']),
+    ...mapState(['droppingShapes', 'gamePaused']),
+  },
+  methods: {
+    ...mapMutations(['getShape', 'toggleSimulation', 'addLeftShape']),
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
+.falling-shapes {
+  position: relative;
+  flex: 1 1 50%;
+}
 </style>
