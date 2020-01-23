a8ae84e0f3a3e0f84eb46c54b0109567ecd41a51
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-23 13:35:48 +0500

    random id for iding the shapes

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,10 @@
+function* getID() {
+  let i = 1;
+  while (true) yield i += 1;
+}
+
 const helpers = {
+  getRandomID: getID(),
   generateRandomNumber(min = 0, max = 1) {
     return min + Math.round(Math.random() * max);
   },
