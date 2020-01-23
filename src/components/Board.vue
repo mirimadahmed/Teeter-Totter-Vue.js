e3c9e98ea8e877cf4e4c134f4553de83373ed5d4
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-23 12:37:11 +0500

    basic designing of the board

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -10,11 +10,26 @@
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
+.teeter-totter {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.board {
+  position: relative;
+  width: 100%;
+  background-color: #0e24a0;
+  transition: 400ms ease-in;
+}
+.foundation {
+  width: 0;
+  height: 0;
+  border-left: 3rem solid transparent;
+  border-right: 3rem solid transparent;
+  border-bottom: 8rem solid #921919;
+}
 </style>
